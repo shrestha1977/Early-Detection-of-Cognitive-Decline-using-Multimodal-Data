bf740b64b6b06bd83dd21e0e3cd9cc78e39cf1e4
Shrestha Mallick <mallickshrestha25@gmail.com>
2026-07-14 20:26:13 +0530

    Update zscore.py

--- risk/zscore.py
+++ risk/zscore.py
@@ -1,10 +1,6 @@
 import joblib
 import os
 
-# -------------------------------------------------------
-# Load Z-score statistics
-# -------------------------------------------------------
-
 BASE_DIR = os.path.dirname(__file__)
 
 stats = joblib.load(
@@ -14,22 +10,7 @@ stats = joblib.load(
 MEAN = stats["mean"]
 STD = stats["std"]
 
-
-# -------------------------------------------------------
-# Calculate Overall Z Score
-# -------------------------------------------------------
-
 def calculate_z(age_adjusted_score):
-
-    """
-    Calculates the final cognitive Z-score.
-
-    Formula:
-
-        Z = (Age Adjusted Score - Mean Age Adjusted Score)
-                /
-          Standard Deviation of Age Adjusted Scores
-    """
 
     if STD == 0:
         return 0
